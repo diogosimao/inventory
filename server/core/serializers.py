b38171b9aa5a4545df84f9de77e0a01d7f01b518
diogosimao <diogosimao@gmail.com>
2017-08-07 00:37:43 -0300

    Initial commit

---
+++ server/core/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Location, Family, Product, Transaction
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ('reference','title', 'description')
+
+
+class FamilySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Family
+        fields = ('reference', 'title', 'description','unit','minQuantity')
+
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('sku','barcode', 'title', 'description','location','family')
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        model = Transaction
+        fields = ('sku', 'barcode','product')
